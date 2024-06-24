30c0b7e58aa60b5322f806effff3e4b25dd18c6c
WOW ROOMS <wowrooms@WOWs-MacBook-Pro.local>
2024-06-24 22:01:48 +0530

    completed project one

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -40,15 +40,34 @@ export default function ContactList() {
 }
 
 const styles = StyleSheet.create({
-    headingText: {},
-    container: {},
-    userCard:{},
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    container: {
+        paddingHorizontal : 16,
+        marginBottom: 4
+    },
+    userCard:{
+        flex:1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 3
+    },
     userImage:{
         width: 60,
         height: 60,
-        borderRadius: 60 /2
+        borderRadius: 60 /2,
+        marginRight: 14
     },
-    userName: {},
-    userStatus:{}
+    userName: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#FFF'
+    },
+    userStatus:{
+        fontSize: 12
+    }
 
 })
