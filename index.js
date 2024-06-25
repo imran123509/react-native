d6a0747d2192ba89fdc01343ac93dd433e4b9977
WOW ROOMS <wowrooms@WOWs-MacBook-Pro.local>
2024-06-25 13:13:56 +0530

    completed pass generated

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {AppRegistry} from 'react-native';
 //import App from './App';
 //import AppPro from './vendor/bundle/ruby/2.6.0/AppPro';
 //import Projectt from './Projectt';
-import Projectt from './vendor/bundle/Projectt';
+//import Projectt from './vendor/bundle/Projectt';
+import PasswordeGenarator from './vendor/bundle/PasswordeGenarator';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Projectt);
+AppRegistry.registerComponent(appName, () => PasswordeGenarator);
